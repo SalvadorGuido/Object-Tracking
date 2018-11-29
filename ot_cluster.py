8d295eb88cd7a9c53d852557e50060326a6221f7
AngelMancilla <a.mancilla@engineer.com>
2018-11-29 11:11:25 -0600

    Creation object tracking cluster class ot_cluster

---
+++ ot_cluster.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 10:28:07 2018
+
+@author: eln.Angel Mancilla
+
+class cluster containing all attributes for each cluster:
+
+f_DistX: distance x from cluster to sensor 
+f_DistY: distance y from cluster to sensor
+f_RangeRate: escalar angular velocity of cluster refering to the sensor
+f_RCS: 
+f_Pdh0: 
+f_Angle: 
+f_SinAngle: 
+f_CosAngle: 
+u_InvalidReasonBitField: 
+u_PropertiesBitField: 
+s_RSPCluIdx: 
+s_NumAssocObjs: 
+iBestAssocObj: 
+
+"""
+
