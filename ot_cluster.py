40029925a8a36b7ec9d8643a7d95237db86481ba
AngelMancilla <a.mancilla@engineer.com>
2018-11-29 14:11:10 -0600

    set_attribute re-defined

--- ot_cluster.py
+++ ot_cluster.py
@@ -23,31 +23,57 @@ iBestAssocObj:
 
 
 """
-
-
 import csv
 import pandas as pd
 import pathlib
 
-        self.f_DistX = None
-        self.f_DistY = None
-        self.f_RangeRate = None
-        self.f_RCS = None
-        self.f_Pdh0 = None
-        self.f_Angle = None
-        self.f_SinAngle = None
-        self.f_CosAngle = None
-        self.u_InvalidReasonBitField = None
-        self.u_PropertiesBitField = None
-        self.s_RSPCluIdx = None
-        self.s_NumAssocObjs = None
-        self.iBestAssocObj = None
-    def set_attribute(self, newdx, newdy, newrrate):
-    	self.f_DistX = newdx
-    	self.f_DistX = newdy	
-    	self.f_RangeRate = newrrate
+
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def __iter__(self):
+        return self.root.__iter__()
+
+class Nodes(object): 
+    def __init__(self,key): 
+        self.left = None
+        self.right = None
+        self.val = key 
     def __str__(self):
-        return "Cluster:dx,xy,rrage:"+str(self.f_DistX)+":"+str(self.f_DistY)+":"+str(self.f_RangeRate)
+        return "Node:(key, left, right) "+str(self.val)+":"+str(self.left)+":"+str(self.right)
+
+class Cluster(object):
+	"""docstring for Cluster"""
+	def __init__(self):
+		self.f_DistX = None
+		self.f_DistY = None
+		self.f_RangeRate = None
+		self.f_RCS = None
+		self.f_Pdh0 = None
+		self.f_Angle = None
+		self.f_SinAngle = None
+		self.f_CosAngle = None
+		self.u_InvalidReasonBitField = None
+		self.u_PropertiesBitField = None
+		self.s_RSPCluIdx = None
+		self.s_NumAssocObjs = None
+		self.iBestAssocObj = None
+	def set_attribute(self, newdx, newdy, newrrate):
+		self.f_DistX = newdx
+		self.f_DistX = newdy
+		self.f_DistX = newrrate
+	def __str__(self):
+		return "Cluster:dx,xy,rrage:"+str(self.f_DistX)+":"+str(self.f_DistY)+":"+str(self.f_RangeRate)
 
 a = Cluster()
 print(a)
+
