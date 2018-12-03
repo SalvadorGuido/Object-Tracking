d3bdbe656c31842f0d078c441ef7c90915711758
lalo2594 <lalo.259472@gmail.com>
2018-12-03 11:30:24 -0600

    Adding acces for the right sensor

--- ReadCsv.py
+++ ReadCsv.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[1]:
 
 
 import numpy as np
@@ -10,13 +10,7 @@ path="D:\Continental\AEP2018\T11.1c_LH_25_DEG_2018.06.27_at_20.53.27_radar-mi_11
 x=pd.read_csv(path)
 
 
-# In[48]:
-
-
-len(x.columns)
-
-
-# In[13]:
+# In[8]:
 
 
 headers=list(x)
@@ -26,18 +20,18 @@ RLTimeStamp=x['SIM EM RIGHT.DataProcCycle.EMGlobalOutput.sSigHeader.uiTimeStamp'
 RLSensor=x[headers[10:((13*385)-1)]]
 RRSensor=x[headers[((13*385)+9):len(x.columns)-7]]
 RLSheaders=RLSensor.columns
-#get information for an specific cluster
-cluster=3
-RLSensor[RLSheaders[(cluster*13)+2:(cluster*13)+15]][1000:1001]
+RRSheaders=RRSensor.columns
+#get information for an specific cluster 
+cluster=4
+RLSensor[RLSheaders[(cluster*13)+2:(cluster*13)+15]][5:8]
+RRSensor[RRSheaders[(cluster*13)+2:(cluster*13)+15]][5:8]
 
 
-# In[20]:
-
-
+# In[9]:
 RLSenValSam=RLSensor.drop_duplicates(subset='LEFT SENSOR.DataProcCycle.EM_ClusterList.u_TimeStamp')
 RRSenValSam=RRSensor.drop_duplicates(subset='RIGHT SENSOR.DataProcCycle.EM_ClusterList.u_TimeStamp')
-print(RRSenValSam)
-#get information for an specific cluster
-cluster=3
-RLSenValSam[RLSheaders[(cluster*13)+2:(cluster*13)+15]][6:7]
+#get information for an specific cluster in the data frame without duplicated samples.
+cluster=0
+RLSenValSam[RLSheaders[(cluster*13)+2:(cluster*13)+3]][6:7]
+RRSenValSam[RRSheaders[(cluster*13)+2:(cluster*13)+3]][6:7]
 
